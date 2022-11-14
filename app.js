aa5e794bff8d5a7a8f1213831ff82be85a830d16
Jay Dawande <83128857+jaydawande@users.noreply.github.com>
2022-11-14 22:56:53 +0530

    Taking input and console

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Scrpt isdd working");
+
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input")
+
+ console.log(txtInput)
+
+function clickHandler() {
+    console.log("clicked!");
+    console.log("input", txtInput.value);
+};
+
+btnTranslate.addEventListener("click", clickHandler)
+ 
