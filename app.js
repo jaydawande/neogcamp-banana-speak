4bc2deddec17f2c44d4a7bd64246090218529bb1
Jay Dawande <83128857+jaydawande@users.noreply.github.com>
2022-11-14 23:45:29 +0530

    input output

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input")
+var outputDiv = document.querySelector("#output")
+ 
 
- console.log(txtInput)
 
 function clickHandler() {
+    outputDiv.innerText = "asdasfasfasds" + txtInput.value
     console.log("clicked!");
     console.log("input", txtInput.value);
 };
